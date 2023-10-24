a3f3d68bd10e440fc9115aa88a697c3d059c44d0
SaezMD <94844441+SaezMD@users.noreply.github.com>
2023-10-24 15:12:04 +0200

    Create reports.py
    clean

---
+++ reports.py
@@ -0,0 +1,37 @@
+"""
+Generate PDF reports named processed.pdf.
+
+Processed Update on <Today's date>
+
+[blank line]
+
+name: Apple
+
+weight: 500 lbs
+
+[blank line]
+
+name: Avocado
+
+weight: 200 lbs
+
+[blank line]
+
+
+"""
+
+#Create a function that can generate a report
+
+#!/usr/bin/env python3
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+
+def generate_report(filename, title, info):
+    styles = getSampleStyleSheet()
+    report = SimpleDocTemplate(filename)
+    report_title = Paragraph(title, styles["h1"])
+    report_info = Paragraph(info, styles["BodyText"])
+    empty_line = Spacer(1,20)
+
+    report.build([report_title, empty_line, report_info, empty_line])
